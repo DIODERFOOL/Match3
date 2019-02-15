6717b21ca73dfeb0ccfb614d6ebce4e3aa928bbb
Alejandro Lopez Martinez <alejandrolm14@gmail.com>
2019-02-15 17:25:29 -0600

    Tile.cs modified

--- Match 3/Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
+++ Match 3/Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
@@ -1,26 +1,4 @@
-﻿/*
- * Copyright (c) 2017 Razeware LLC
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -49,5 +27,36 @@ public class Tile : MonoBehaviour {
 		render.color = Color.white;
 		previousSelected = null;
 	}
+
+	void OnMouseDown() {
+		// 1
+		if (render.sprite == null || BoardManager.instance.IsShifting) {
+			return;
+		}
+
+		if (isSelected) { // 2 Is it already selected?
+			Deselect();
+		} else {
+			if (previousSelected == null) { // 3 Is it the first tile selected?
+				Select();
+			} else {
+				SwapSprite(previousSelected.render);
+				previousSelected.Deselect(); // 4
+			}
+		}
+	}
+
+
+	public void SwapSprite(SpriteRenderer render2) {
+		if (render.sprite == render2.sprite) {
+			return;
+		}
+
+		Sprite tempSprite = render2.sprite;
+		render2.sprite = render.sprite;
+		render.sprite = tempSprite;
+		SFXManager.instance.PlaySFX(Clip.Swap);
+	}
+
 
 }
