9bb931b0d3dbd0732efd1014c29840e34d334ad6
Alejandro Lopez Martinez <alejandrolm14@gmail.com>
2019-02-15 17:43:56 -0600

    Update Tile.cs

--- Match 3/Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
+++ Match 3/Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
@@ -8,6 +8,7 @@ public class Tile : MonoBehaviour {
 
 	private SpriteRenderer render;
 	private bool isSelected = false;
+	private bool matchFound = false;
 
 	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
 
@@ -29,26 +30,33 @@ public class Tile : MonoBehaviour {
 	}
 
 	void OnMouseDown() {
-		// 1
+		
 		if (render.sprite == null || BoardManager.instance.IsShifting) {
 			return;
 		}
 
-		if (isSelected) { // 2 Is it already selected?
+		if (isSelected) { 
 			Deselect();
 		} else {
-			if (previousSelected == null) { // 3 Is it the first tile selected?
+			if (previousSelected == null) { 
 				Select();
 			}
             else
             {
                 if (GetAllAdjacentTiles().Contains(previousSelected.gameObject))
-                { // 1
-                    SwapSprite(previousSelected.render); // 2
-                    previousSelected.Deselect();
+                { 
+	                if (GetAllAdjacentTiles().Contains(previousSelected.gameObject)) { 
+		                SwapSprite(previousSelected.render); 
+		                previousSelected.ClearAllMatches();
+		                previousSelected.Deselect();
+		                ClearAllMatches();
+	                } else { 
+		                previousSelected.GetComponent<Tile>().Deselect();
+		                Select();
+	                }
                 }
                 else
-                { // 3
+                { 
                     previousSelected.GetComponent<Tile>().Deselect();
                     Select();
                 }
@@ -78,14 +86,53 @@ public class Tile : MonoBehaviour {
         return null;
     }
 
-    private List<GameObject> GetAllAdjacentTiles()
-    {
-        List<GameObject> adjacentTiles = new List<GameObject>();
-        for (int i = 0; i < adjacentDirections.Length; i++)
-        {
-            adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
-        }
-        return adjacentTiles;
-    }
+	private List<GameObject> GetAllAdjacentTiles() {
+		List<GameObject> adjacentTiles = new List<GameObject>();
+		for (int i = 0; i < adjacentDirections.Length; i++) {
+			adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+		}
+		return adjacentTiles;
+	}
+	
+	private List<GameObject> FindMatch(Vector2 castDir) { // 1
+		List<GameObject> matchingTiles = new List<GameObject>(); // 2
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir); // 3
+		while (hit.collider != null && hit.collider.GetComponent<SpriteRenderer>().sprite == render.sprite) { // 4
+			matchingTiles.Add(hit.collider.gameObject);
+			hit = Physics2D.Raycast(hit.collider.transform.position, castDir);
+		}
+		return matchingTiles; // 5
+	}
+
+	private void ClearMatch(Vector2[] paths) // 1
+	{
+		List<GameObject> matchingTiles = new List<GameObject>(); // 2
+		for (int i = 0; i < paths.Length; i++) // 3
+		{
+			matchingTiles.AddRange(FindMatch(paths[i]));
+		}
+		if (matchingTiles.Count >= 2) // 4
+		{
+			for (int i = 0; i < matchingTiles.Count; i++) // 5
+			{
+				matchingTiles[i].GetComponent<SpriteRenderer>().sprite = null;
+			}
+			matchFound = true; // 6
+		}
+	}
+	
+	public void ClearAllMatches() {
+		if (render.sprite == null)
+			return;
+
+		ClearMatch(new Vector2[2] { Vector2.left, Vector2.right });
+		ClearMatch(new Vector2[2] { Vector2.up, Vector2.down });
+		if (matchFound) {
+			render.sprite = null;
+			matchFound = false;
+			SFXManager.instance.PlaySFX(Clip.Clear);
+		}
+	}
+
 
 }
