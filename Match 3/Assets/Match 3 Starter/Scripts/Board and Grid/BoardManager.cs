6e93a08f94554f9fa43f13e22efe7771c5fea12c
Rafael Rojas <yugnarcorp@hotmail.com>
2019-02-15 18:08:04 -0600

    Uno q funciona aún más
    xd

--- Match 3/Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
+++ Match 3/Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
@@ -24,39 +24,49 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class BoardManager : MonoBehaviour {
-	public static BoardManager instance;
-	public List<Sprite> characters = new List<Sprite>();
-	public GameObject tile;
-	public int xSize, ySize;
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager instance;
+    public List<Sprite> characters = new List<Sprite>();
+    public GameObject tile;
+    public int xSize, ySize;
 
-	private GameObject[,] tiles;
+    private GameObject[,] tiles;
 
-	public bool IsShifting { get; set; }
+    public bool IsShifting { get; set; }
 
-	void Start () {
-		instance = GetComponent<BoardManager>();
+    void Start()
+    {
+        instance = GetComponent<BoardManager>();
 
-		Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+        Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
         CreateBoard(offset.x, offset.y);
     }
 
-	private void CreateBoard (float xOffset, float yOffset) {
-		tiles = new GameObject[xSize, ySize];
+    private void CreateBoard(float xOffset, float yOffset)
+    {
+        tiles = new GameObject[xSize, ySize];
 
         float startX = transform.position.x;
-		float startY = transform.position.y;
-        Sprite[] previousLeft = new Sprite[ySize];
-        Sprite previousBelow = null;
-        for (int x = 0; x < xSize; x++) {
-			for (int y = 0; y < ySize; y++) {
-				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
-				tiles[x, y] = newTile;
-                newTile.transform.parent = transform;
-                List<Sprite> possibleCharacters = new List<Sprite>(); // 1
-                possibleCharacters.AddRange(characters); // 2
-                possibleCharacters.Remove(previousLeft[y]); // 3
+        float startY = transform.position.y;
+
+        Sprite[] previousLeft = new Sprite[ySize]; // Add this line
+        Sprite previousBelow = null; // Add this line
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
+                tiles[x, y] = newTile;
+                newTile.transform.parent = transform; // Add this line
+
+                List<Sprite> possibleCharacters = new List<Sprite>();
+                possibleCharacters.AddRange(characters);
+
+                possibleCharacters.Remove(previousLeft[y]);
                 possibleCharacters.Remove(previousBelow);
+
                 Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
                 newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
                 previousLeft[y] = newSprite;
@@ -78,6 +88,14 @@ public class BoardManager : MonoBehaviour {
                 }
             }
         }
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                tiles[x, y].GetComponent<Tile>().ClearAllMatches();
+            }
+        }
     }
 
     private IEnumerator ShiftTilesDown(int x, int yStart, float shiftDelay = .03f)
@@ -87,20 +105,20 @@ public class BoardManager : MonoBehaviour {
         int nullCount = 0;
 
         for (int y = yStart; y < ySize; y++)
-        {  // 1
+        {
             SpriteRenderer render = tiles[x, y].GetComponent<SpriteRenderer>();
             if (render.sprite == null)
-            { // 2
+            {
                 nullCount++;
             }
             renders.Add(render);
         }
 
         for (int i = 0; i < nullCount; i++)
-        { // 3
-            yield return new WaitForSeconds(shiftDelay);// 4
+        {
+            yield return new WaitForSeconds(shiftDelay);
             for (int k = 0; k < renders.Count - 1; k++)
-            { // 5
+            {
                 renders[k].sprite = renders[k + 1].sprite;
                 renders[k + 1].sprite = GetNewSprite(x, ySize - 1);
             }
